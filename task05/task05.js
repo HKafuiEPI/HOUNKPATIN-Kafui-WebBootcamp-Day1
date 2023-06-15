f7f1daa5c9214b70627c7b5c61cd9098ed75eb01
HKafuiEPI <kafui.hounkpatin@epitech.eu>
2023-06-15 11:07:36 +0100

    adding Task05

---
+++ task05/task05.js
@@ -0,0 +1,23 @@
+function isPalindrome(str) {
+    str = str.toLowerCase();
+    let a = 0;
+    let i = 0, v = 0, nv = "";
+    for (let j = 0; j < str.length; j++){
+        if (str[j] >= 'a' && str[j] <= 'z'){
+            nv += str[j]; 
+        }
+    }
+    a = nv.length - 1;
+    for (i = 0; i < nv.length && a >= 0; i++){
+        if (nv[i] == nv[a]){
+            v++;a--;
+        }
+    }
+    if (v == nv.length) return true;
+    else return false;
+    
+}
+
+const str = "A man, a plan, a canal: Panama";
+const isPalindromeString = isPalindrome(str);
+console.log(`Is "${str}" a palindrome? ${isPalindromeString}`);
