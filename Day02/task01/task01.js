8be742d12223ed58aa9fd79da324387f5380fd05
HKafuiEPI <kafui.hounkpatin@epitech.eu>
2023-06-16 12:45:33 +0100

    removing loops

--- Day02/task01/task01.js
+++ Day02/task01/task01.js
@@ -1,15 +1,14 @@
+function my_sum(total, value) {
+    if (value % 2 != 0)
+        return total + value;
+    else
+        return total;
+}
+
 function sumOfOddNumbers(n){
-    let r = 0;
-    for (let i = 0; i < n.length; i++){
-        if (n[i] % 2 != 0) r += n[i];
-    }
-    return r;
+    return r = n.reduce(my_sum);
 }
 
 module.exports = {
 	sumOfOddNumbers
 }
-
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const oddSum = sumOfOddNumbers(numbers);
-console.log(oddSum);
